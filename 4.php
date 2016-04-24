88d44e93badcc8154de262fa7068c8c3efa0fd81
Luis Montealegre <montealegreluis@gmail.com>
2016-04-23 22:28:00 -0500

    Improve the solution for challenge #4.

--- 4.php
+++ 4.php
@@ -1,11 +1,8 @@
 <?php
 $number = $argv[1];
 
-$count = 0;
-for ($i = 1; $i <= $number; $i++) {
-   if (strpos((string) $i, '0') !== false) {
-       $count++;
-   }
-}
+$count = count(array_filter(range(1, $number), function ($i) {
+   return strpos($i, '0') !== false;
+}));
 
 echo "The amount of numbers that contain 0 as digit between 1 and $number is $count", PHP_EOL;
