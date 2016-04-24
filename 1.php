c2bb4c6b20d9ab72d4095779a910dd470b710636
Luis Montealegre <montealegreluis@gmail.com>
2016-04-24 00:16:54 -0500

    Improve challenge #1.

--- 1.php
+++ 1.php
@@ -2,11 +2,8 @@
 $start = $argv[1];
 $end = $argv[2];
 
-$count = 0;
-for ($i = $start; $i <= $end; $i++) {
-   if (sqrt($i) == number_format(sqrt($i))) {
-       $count++;
-   }
-}
+$count = count(array_filter(range($start, $end), function ($i) {
+    return sqrt($i) == number_format(sqrt($i));
+}));
 
 echo "The amount of perfect squares between $start and $end is $count", PHP_EOL;
