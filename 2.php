7a82830766c7fdb4d8726f9c20979729fec66fc2
Luis Montealegre <montealegreluis@gmail.com>
2016-04-29 21:38:31 -0500

    Remove redundant assignment from challenge #2.

--- 2.php
+++ 2.php
@@ -2,7 +2,7 @@
 $number = $argv[1];
 
 $sum = array_reduce(range(1, $number), function ($carry, $i) {
-   return $carry += array_sum(str_split($i));
+   return $carry + array_sum(str_split($i));
 });
 
 echo "The sum of digits between 1 and $number is $sum", PHP_EOL;
